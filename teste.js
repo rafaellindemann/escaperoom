2f1caa953cd5aa089d15b8a8c0445ff1ccc18132
rafaellindemann <rafaelld@gmail.com>
2026-03-24 16:43:09 -0300

    add mvp escape room

---
+++ teste.js
@@ -0,0 +1,79 @@
+const perguntas = [
+    {
+        id:0,
+        img: './imgs/0.jpg',
+        hash: 'alguma coisa gerada a partir dos valores das props deste objeto'
+    },
+    {
+        id:1,
+        img: './imgs/0.jpg',
+        hash: 'alguma coisa gerada a partir dos valores das props deste objeto'
+    },
+    {
+        id:2,
+        img: './imgs/0.jpg',
+        hash: 'alguma coisa gerada a partir dos valores das props deste objeto'
+    },
+    {
+        id:3,
+        img: './imgs/0.jpg',
+        hash: 'alguma coisa gerada a partir dos valores das props deste objeto'
+    },
+]
+
+
+
+const p = [
+    {
+        id:0,
+        img: './imgs/0.jpg',
+    },
+    {
+        id:1,
+        img: './imgs/0.jpg',
+    },
+    {
+        id:2,
+        img: './imgs/0.jpg',
+    },
+    {
+        id:3,
+        img: './imgs/0.jpg',
+    },
+]
+
+const b = [2, 1, 0, 3]
+const r = [499, 65, 900, 14]
+
+
+// const q = [
+//     {
+//         id: 0,
+//         img: './imgs/nomeDoArquivo.jpg',
+//         r: ''
+//     },
+// ]
+
+
+
+const q = [
+    { id: 0, img: './imgs/a003.jpg', r: ['b'], obs: 'Digite a letra da opção' },
+    { id: 2, img: './imgs/a008.jpg', r: ['f'], obs: 'Digite a letra da opção' },
+    { id: 3, img: './imgs/a039.jpg', r: ['60'], obs: '' },
+    { id: 4, img: './imgs/a043.jpg', r: ['4'], obs: '' },
+    { id: 5, img: './imgs/a083.jpg', r: ['66'], obs: '' },
+    { id: 7, img: './imgs/a138.jpg', r: ['75'], obs: '' },
+    { id: 9, img: './imgs/b020.jpg', r: ['0.5'], obs: 'Resposta em formato decimal. Ex: "0.2" representaria 1/5 '},
+    { id: 10, img: './imgs/b028.jpg', r: ['2'], obs: 'Resposta em minutos' },
+    { id: 11, img: './imgs/b035.jpg', r: ['135'], obs: 'Resposta em minutos' },
+    { id: 12, img: './imgs/b036.jpg', r: ['2'], obs: '' },
+    { id: 14, img: './imgs/b056.jpg', r: ['80'], obs: '' },
+    { id: 15, img: './imgs/b057.jpg', r: ['360'], obs: '' },
+    { id: 16, img: './imgs/b062.jpg', r: ['1'], obs: '' },
+    { id: 17, img: './imgs/b079.jpg', r: ['3800'], obs: '' },
+    { id: 18, img: './imgs/b084.jpg', r: ['5'], obs: '' },
+    { id: 19, img: './imgs/b089.jpg', r: ['21'], obs: '' },
+    { id: 20, img: './imgs/b103.jpg', r: ['3'], obs: '' },
+    { id: 21, img: './imgs/b122.jpg', r: ['81'], obs: '' },
+    { id: 22, img: './imgs/b141.jpg', r: ['1'], obs: 'Resposta em metros' },
+];
